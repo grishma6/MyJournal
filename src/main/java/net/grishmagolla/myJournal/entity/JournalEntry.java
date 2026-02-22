a1fabc8769d41e0f7538431c67a99cc1bf840a98
grishma6 <gxg59150@ucmo.edu>
2026-02-21 19:06:29 -0800

    project1

--- src/main/java/net/grishmagolla/myJournal/entity/JournalEntry.java
+++ src/main/java/net/grishmagolla/myJournal/entity/JournalEntry.java
@@ -1,4 +1,28 @@
 package net.grishmagolla.myJournal.entity;
 
 public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+
+    public long getId(long id){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+     }
+
+     public String getTitle(String title){
+        return title;
+     }
+     public void setTitle(){
+        this.title = title;
+     }
+     public String getContent(){
+        return content;
+     }
+     public void setContent(String content){
+        this.content = content;
+     }
 }
+
