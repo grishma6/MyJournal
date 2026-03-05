4ee2aafc9a6f5956c0e3252ad54720087548bf92
grishma6 <gxg59150@ucmo.edu>
2026-03-04 16:11:11 -0800

    Lombok

--- src/main/java/net/grishmagolla/myJournal/entity/JournalEntry.java
+++ src/main/java/net/grishmagolla/myJournal/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package net.grishmagolla.myJournal.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,45 +11,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    private LocalDateTime date;
-
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
 }
 
